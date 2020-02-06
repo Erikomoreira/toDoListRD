fdbeb069d491fb8bdcdd2c45c165c9efb1e683c6
Erik Moreira <erik.oliveira.moreira@hotmail.com>
2020-02-06 16:54:59 -0300

    Finalização do projeto

--- js/script.js
+++ js/script.js
@@ -1,65 +1,92 @@
 let btnAddTask = document.getElementById("adicionar-tarefa");
-console.log(btnAddTask);
 let tarefas = document.querySelector("#tarefas");
 let inpuTarefa = document.querySelector("#tarefa-digitada")
-let finalizarTarefa = document.querySelector(".botao-tarefa");
+let listaTarefas = localStorage.getItem('listaTarefas') ? JSON.parse(localStorage.getItem('listaTarefas')) : [];
 
+const salvarLocalStorage = (listaTarefas) => {
+    let tarefasEmJson = JSON.stringify(listaTarefas);
+    localStorage.setItem('listaTarefas', tarefasEmJson);
+    console.log("Lista de tarefas salva com sucesso")
+}
 
-finalizarTarefa.onclick = function (event) {
+const mostrarNaTela = (listaTarefas) => {
 
-    remove(event.target[0]);
+    listaTarefas.forEach((textoTarefa, item) => {
+
+        let tarefaNova = ` <div class="col-md-4">
+
+            <div class="card-tarefa">
+                <div class="texto-tarefa">
+                ${textoTarefa}
+                </div>
+                <div class="botao-tarefa">
+                    <img width="32px" src="img/check.png" alt="Botão para finalizar tarefa"
+                        title="Botão para finalizar tarefa">
+                </div>
+            </div>
+            </div> `
+
+            tarefas.insertAdjacentHTML('beforeend', tarefaNova)
+            let objTarefaNova = tarefas.lastElementChild.lastElementChild.lastElementChild;
+            objTarefaNova.onclick = (event) => {
+
+                event.target.parentNode.parentNode.parentNode.remove();
+                listaTarefas =  listaTarefas.filter((valor, index) => valor != textoTarefa)
+                    salvarLocalStorage(listaTarefas);
+            }
+    });
 
 }
 
-btnAddTask.onclick = function () {
+mostrarNaTela(listaTarefas);
 
-    let valorDigitado = inpuTarefa.value;
+const criarTarefaComEnter = (event) => {
 
-    let tarefaNova = ` <div class="col-md-4">
-
-    <div class="card-tarefa">
-        <div class="texto-tarefa">
-           ${valorDigitado}
-        </div>
-        <div class="botao-tarefa">
-            <img width="32px" src="img/check.png" alt="Botão para finalizar tarefa"
-                title="Botão para finalizar tarefa">
-        </div>
-    </div>
-
-    </div> `;
-
-
-    tarefas.innerHTML += tarefaNova;
-
-}
-
-
-inpuTarefa.addEventListener("keyup", function (event) {
-
-    if (event.keyCode === 13) {
+    if (event.keyCode == 13 || event.type == "click") {
 
         let valorDigitado = inpuTarefa.value;
 
-        let tarefaNova = ` <div class="col-md-4">
-    
-        <div class="card-tarefa">
-            <div class="texto-tarefa">
-               ${valorDigitado}
+        if (valorDigitado == "") {
+
+            return alert("Você deve digitar uma tarefa primeiro");
+
+        } else {
+
+            listaTarefas.push(valorDigitado);
+            salvarLocalStorage(listaTarefas);
+
+            inpuTarefa.value = "";
+
+            let tarefaNova = ` <div class="col-md-4">
+
+            <div class="card-tarefa">
+                <div class="texto-tarefa">
+                ${valorDigitado}
+                </div>
+                <div class="botao-tarefa">
+                    <img width="32px" src="img/check.png" alt="Botão para finalizar tarefa"
+                        title="Botão para finalizar tarefa">
+                </div>
             </div>
-            <div class="botao-tarefa">
-                <img width="32px" src="img/check.png" alt="Botão para finalizar tarefa"
-                    title="Botão para finalizar tarefa">
-            </div>
-        </div>
-    
-        </div> `;
 
+            </div> `;
 
-        tarefas.innerHTML += tarefaNova;
+            tarefas.insertAdjacentHTML('beforeend', tarefaNova)
 
+            let objTarefaNova = tarefas.lastElementChild.lastElementChild.lastElementChild;
 
-    }
-});
+            objTarefaNova.onclick = (event) => {
 
+                event.target.parentNode.parentNode.parentNode.remove();
+                listaTarefas = listaTarefas.filter(valor => valor != valorDigitado)
+                    salvarLocalStorage(listaTarefas);
+                }
+                    
+            }
+        }
+
+}
+
+inpuTarefa.addEventListener("keypress", criarTarefaComEnter)
+btnAddTask.addEventListener("click", criarTarefaComEnter)
 
